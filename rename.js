7a4971fbee0cb9600db8a70fd2a0c22bd0acb02b
pgpv <pedrogpvilaca@gmail.com>
2017-05-22 12:53:40 +0200

    setting up

---
+++ rename.js
@@ -0,0 +1,22 @@
+function rename( data, oldName, newName )
+{
+  if( Array.isArray( data ) )
+  {
+    for ( let i = 0; i < data.length; i++ )
+    {
+      if ( data[ i ].hasOwnProperty( oldName ) )
+      {
+        data[ i ][ newName ] = data[ i ][ oldName ];
+        delete data[ i ][ oldName ];
+      }
+    }
+  }
+  else
+  {
+    if ( data.hasOwnProperty( oldName ) )
+    {
+      data[ newName ] = data[ oldName ];
+      delete data[ oldName ];
+    }
+  }
+}
